2df5f0f8af6d7bcbfdfe932c36ab0de4643bac7e
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-03-08 05:11:58 -0500

    Templates working

--- webtelemetry.js
+++ webtelemetry.js
@@ -1,21 +1,18 @@
 Tests = new Mongo.Collection("timeseries");
 
 if (Meteor.isClient) {
-    Template.test_items.helpers({
+    Template.test_item_list.helpers({
         'test_names': function () { return Tests.find(); }
     });
 
     Template.test_data.helpers({
-        'test_values': function () { 
-            return  {'name': Session.get('selected_text')}
-             }
+        'test_values': function () { return ({'name': Session.get('selected_text')}); }
     });
 
     Template.body.events({
         'change.test-dropdown': function (event) {
-        //Session.set('selected_text', event.target.value); 
-        Session.set('selected_text', 'helllloo'); 
+            Session.set('selected_text', event.target.value); 
         }
     });
-
+    
 }
