4c28eba8ae7916ac32c0438552adbf673a6b70f2
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-03-08 02:11:35 +0000

    tried new syntax, two templates

--- webtelemetry.js
+++ webtelemetry.js
@@ -1,12 +1,21 @@
 Tests = new Mongo.Collection("timeseries");
 
 if (Meteor.isClient) {
-    Template.testlist.helpers({
-        tests: function () {
-            return Tests.find();
-        },
-        datatable: function () {
-            return Tests.find();
+    Template.test_items.helpers({
+        'test_names': function () { return Tests.find(); }
+    });
+
+    Template.test_data.helpers({
+        'test_values': function () { 
+            return  {'name': Session.get('selected_text')}
+             }
+    });
+
+    Template.body.events({
+        'change.test-dropdown': function (event) {
+        //Session.set('selected_text', event.target.value); 
+        Session.set('selected_text', 'helllloo'); 
         }
     });
+
 }
