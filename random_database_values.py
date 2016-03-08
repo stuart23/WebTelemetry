ae64c7f37b9eb799ecc54a7922c230a8f63056fb
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-03-08 22:13:03 +0000

    added multiple tracks

--- random_database_values.py
+++ random_database_values.py
@@ -9,17 +9,16 @@ def __main__(*args, **kwargs):
     """
     client = MongoClient('localhost', 3001)
     timeseries_collection = client.meteor.timeseries
-
+    timeseries_collection.drop()
     starttime = datetime.now()
-    random_series = lambda multiplier: [{'time': starttime + timedelta(0,0,1000*n), 'value': multiplier*random()} for n in range(1000)]
-    timeseries_collection.insert({'name': 'spa',
+    random_series = lambda multiplier, rate: [{'time': starttime + timedelta(0,0,1000000*n/rate), 'value': multiplier*random()} for n in range(1000)]
+    for track in ['silverstone','spa', 'monaco']:
+        print                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   timeseries_collection.insert({'name': track,
                                   'timeseries': [
-                                                 {'name': 'velocity', 'units': 'm/s', 'data': random_series(100.)},
-                                                 {'name': 'rpm', 'units': '1/min', 'data': random_series(5000.)},
+                                                 {'name': 'velocity', 'units': 'm/s', 'data': random_series(100., 50.)},
+                                                 {'name': 'rpm', 'units': '1/min', 'data': random_series(5000., 200.)},
                                                 ]
                                 })
 
 if __name__ == '__main__':
     __main__()
-
-
