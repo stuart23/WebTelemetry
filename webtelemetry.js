539c19be8160c14ce82dcdb53c2c227245990a8f
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-03-08 00:17:02 +0000

    typo

--- webtelemetry.js
+++ webtelemetry.js
@@ -1,18 +1,9 @@
-Tests = new Mongo.collection("timeseries");
+Tests = new Mongo.Collection("timeseries");
 
 if (Meteor.isClient) {
     Template.body.helpers({
         tests: function () {
             return Tests.find();
         }
-//        test_data: function() {
-//            return Tests.find({"name": Session.get("selected_test")});
-//        }
     });
-//
-//    Template.body.events({
-//        "change .test-dropdown": function(event) {
-//            Session.set("selected_test", event.target.value);
-//        }
-//    });
 }
