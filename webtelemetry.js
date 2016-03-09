a17ca01060755017312c930436907f74d9c94491
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-03-09 23:04:34 +0000

    tried presetting Session vars

--- webtelemetry.js
+++ webtelemetry.js
@@ -21,9 +21,11 @@ function resample(event_id, frequency) {
   var event_datasets = Timeseries.find(
     {event : new Meteor.Collection.ObjectID(event_id)}
   );
-  var time_max_min = timeMaxMin(event_datasets);
+  var ee = 2;
+  var ff = 4;
+  // var time_max_min = timeMaxMin(event_datasets);
   // Times in milliseconds
-  var j = Session.get('selected_frequency');
+  var j = frequency;
   var k = j + 5;
   var l = j * 2;
   time_delta = time_max_min.time_max-time_max_min.time_min;
@@ -33,6 +35,9 @@ function resample(event_id, frequency) {
 }
 
 if (Meteor.isClient) {
+  Session.set('selected_text', null);
+  Session.set('selected_frequency', null);
+
     Template.test_item_list.helpers({
         'test_names': function () {
           return Events.find().map(
@@ -45,12 +50,19 @@ if (Meteor.isClient) {
 
     Template.test_data.helpers({
         'test_values': function () {
-          // if {Session.get('selected_test') == null}
-          return resample(
-                                  Session.get('selected_test'),
-                                  Session.get('selected_frequency')
-                                  );
-                                }
+          var a = Session.get('selected_test');
+          var b = Number(Session.get('selected_frequency'));
+          var c = Session.get('selected_frequency');
+          var d = Number(Session.get('selected_frequency'));
+          if ((Session.get('selected_test')) && (Session.get('selected_frequency'))) {
+            return resample(
+              Session.get('selected_test'),
+              Number(Session.get('selected_frequency'))
+              );
+            } else {
+              return false;
+            }
+          }
     });
 
     Template.body.events({
