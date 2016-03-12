6a26594ea3d68080b678c3fa2df9bf11de51a154
Stuart Buckingham <stuart.buckingham@gmail.com>
2016-03-12 14:04:55 +0000

    about to merge templates

--- webtelemetry.js
+++ webtelemetry.js
@@ -18,16 +18,13 @@ function timeMaxMin(event_datasets) {
 }
 
 function resample(event_id, frequency) {
+  console.log(event_id);
+  console.log(frequency);
   var event_datasets = Timeseries.find(
     {event : new Meteor.Collection.ObjectID(event_id)}
   );
-  var ee = 2;
-  var ff = 4;
-  // var time_max_min = timeMaxMin(event_datasets);
+  var time_max_min = timeMaxMin(event_datasets);
   // Times in milliseconds
-  var j = frequency;
-  var k = j + 5;
-  var l = j * 2;
   time_delta = time_max_min.time_max-time_max_min.time_min;
 
   return {test_value: [frequency, time_max_min.time_max-time_max_min.time_min,
@@ -35,9 +32,6 @@ function resample(event_id, frequency) {
 }
 
 if (Meteor.isClient) {
-  Session.set('selected_text', null);
-  Session.set('selected_frequency', null);
-
     Template.test_item_list.helpers({
         'test_names': function () {
           return Events.find().map(
@@ -51,10 +45,9 @@ if (Meteor.isClient) {
     Template.test_data.helpers({
         'test_values': function () {
           var a = Session.get('selected_test');
-          var b = Number(Session.get('selected_frequency'));
           var c = Session.get('selected_frequency');
           var d = Number(Session.get('selected_frequency'));
-          if ((Session.get('selected_test')) && (Session.get('selected_frequency'))) {
+          if ((Session.get('selected_test') != '') && (Session.get('selected_frequency') != '')) {
             return resample(
               Session.get('selected_test'),
               Number(Session.get('selected_frequency'))
@@ -66,13 +59,13 @@ if (Meteor.isClient) {
     });
 
     Template.body.events({
-        'change.test-dropdown': function (event) {
-            Session.set('selected_test', event.target.value);
-        },
+      'change.test-dropdown': function (event) {
+          Session.set('selected_test', event.target.value);
+      },
 
-        'change.frequency-selector': function (event) {
-            Session.set('selected_frequency', event.target.value);
-        }
+      'change.frequency-selector': function (event) {
+          Session.set('selected_frequency', event.target.value);
+      }
     });
 
 }
